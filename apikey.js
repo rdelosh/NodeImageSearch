2f6406959fd981d26791ff029a9f52bc1fdc8cbb
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2018-03-30 03:11:12 -0800

    preparing push for heroku

---
+++ apikey.js
@@ -0,0 +1,3 @@
+const url_key = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCP8aM7L4eHJkI4jWl8RE-qMZX2qgYUr_Y&cx=017597253757306135990:t4hijwiwgt4&searchType=image&q='
+
+module.exports = url_key
